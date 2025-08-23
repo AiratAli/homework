b9eeb28bec805fa0b30e70da193574ccbbdeb0b9
AiratAli <airat.aliullin@yandex.ru>
2025-08-23 15:59:49 +0400

    lesson16

---
+++ lesson16/main.js
@@ -0,0 +1,61 @@
+const user = {
+    Airat: {
+        name: 'Airat',
+        age: 37,
+        height: 176,
+        weight: 80
+    },
+    Danya: {
+        sayHello(name) {
+            console.log(`Hello ${name}`)
+        }
+    }
+}
+
+console.log(user.Airat)
+user.Danya.sayHello('Danya')
+
+/*===========================*/
+
+const users = [
+    {
+        name: 'Anna',
+        age: 20,
+        isAdmin: true
+    },
+    {
+        name: 'Tom',
+        age: 25,
+        isAdmin: false
+    },
+    {
+        name: 'Ivan',
+        age: 23,
+        isAdmin: false
+    },
+    {
+        name: 'Dmitry',
+        age: 26,
+        isAdmin: true
+    },
+    {
+        name: 'Olga',
+        age: 18,
+        isAdmin: false
+    },
+    {
+        name: 'Lili',
+        age: 25,
+        isAdmin: false
+    }
+];
+
+let NumberOfOrdinaryUsers = 0;
+
+for (let i = 0; i < users.length; i++) {
+    if (!users[i].isAdmin) {
+        NumberOfOrdinaryUsers++;
+    }
+}
+
+console.log("Количество простых пользователей:", NumberOfOrdinaryUsers);
